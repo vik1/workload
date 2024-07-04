cc0a57a62823d8f2071f83d4272ad8579fafa6e9
Vikrant Verma <vikrantv@infinite.com>
2024-07-04 19:33:44 +0530

    first

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t ibmicpcoc/qod-api:1.1.0 .
-docker tag ibmicpcoc/qod-api:1.1.0 ibmicpcoc/qod-api:latest
-docker push ibmicpcoc/qod-api:1.1.0
-docker push ibmicpcoc/qod-api:latest
+docker build -t ibmicpcoc/test-workload:1.1.0 .
+docker tag ibmicpcoc/test-workload:1.1.0 ibmicpcoc/test-workload:latest
+docker push ibmicpcoc/test-workload:1.1.0
+docker push ibmicpcoc/test-workload:latest
